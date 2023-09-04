51b61584f08391abb45f9363004e4cad05b78c7c
Hrithik <slumdog4321@gmail.com>
2023-09-04 20:04:27 +0530

    custom user location marker

--- app/components/Home/GooglemapView.js
+++ app/components/Home/GooglemapView.js
@@ -1,16 +1,30 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import React from "react";
+import { UserLocationContext } from "@/context/UserLocationContext";
+import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
+import React, { useContext } from "react";
 
 const GooglemapView = () => {
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
   const ContainerStyle = {
     width: "100%",
     height: "70vh",
   };
   const coordinates = { lat: 22.484619, lng: 88.357903 };
+  console.log(userLocation);
   return (
     <div>
       <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
-        <GoogleMap mapContainerStyle={ContainerStyle} center={coordinates} zoom={13}></GoogleMap>
+        <GoogleMap mapContainerStyle={ContainerStyle} center={userLocation} zoom={13}>
+          <MarkerF
+            position={userLocation}
+            icon={{
+              url: "/user-location.png",
+              scaledSize: {
+                width: 50,
+                height: 50,
+              },
+            }}
+          />
+        </GoogleMap>
       </LoadScript>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,10 @@
+"use client";
+import { useEffect, useState } from "react";
 import Provider from "./Provider";
 import HeaderNavbar from "./components/HeaderNavbar";
 import "./globals.css";
 import { Dosis } from "next/font/google";
+import { UserLocationContext } from "@/context/UserLocationContext";
 
 const dosis = Dosis({ subsets: ["latin"] });
 
@@ -11,12 +14,27 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const [userLocation, setUserLocation] = useState([]);
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      console.log(pos);
+      setUserLocation({
+        lat: pos.coords.latitude,
+        lng: pos.coords.longitude,
+      });
+    });
+  };
   return (
     <html lang="en">
       <body className={dosis.className}>
         <Provider>
-          <HeaderNavbar />
-          {children}
+          <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+            <HeaderNavbar />
+            {children}
+          </UserLocationContext.Provider>
         </Provider>
       </body>
     </html>
